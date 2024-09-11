4551ddd7b7c7a86f2f2320c2ad74156e0e1b4070
SHubham SIngh <164128999+Shubhambth@users.noreply.github.com>
2024-09-11 10:29:14 +0000

    added my sql data base

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+#postgresql://postgres:nnbIljAAifNmgNDdimALevlSMdyOrBOM@autorack.proxy.rlwy.net:56083/railway
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'nnbIljAAifNmgNDdimALevlSMdyOrBOM',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '56083',
     }
 }
 
