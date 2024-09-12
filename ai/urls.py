03a603463b80251c174cecf1b2e51561be8f5ff4
SHubham SIngh <164128999+Shubhambth@users.noreply.github.com>
2024-09-12 07:55:52 +0000

    login and signup

--- ai/urls.py
+++ ai/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import chatbot , index , features
+from .views import chatbot , index , features , login_view , register_view , user_logout
 
 urlpatterns = [
     path('chat/', chatbot, name='chatbot'),
     path('', index , name='index'),
     path('features/',features,name='features'),
+    path('login/',login_view,name='login'),
+    path('register/',register_view, name='register'),
+    path('logout/', user_logout, name='logout')
 ]
--- ai/views.py
+++ ai/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 import g4f
 from .models import Chats
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import auth
+from django.contrib.auth.decorators import login_required
 
+User = get_user_model()
+
+def login_view(request):
+    if request.method== "POST":
+        username = request.POST.get("username") 
+        password = request.POST.get("password") 
+        if all([username,password]):
+          user = authenticate(request, username=username, password=password)
+          if user is not None:
+              login(request, user)
+              return redirect('chatbot')
+    return render(request,'login_view.html')
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+        
+        try:
+          user = User.objects.create_user(username=username,email=email,password=password)
+          user.save()
+          return redirect('login')
+        except:
+          return render(request,'signup_view.html',{})
+      
+    return render(request,'signup_view.html')
 
 def index(request):
     return render(request , 'index.html')
@@ -11,7 +43,7 @@ def features(request):
     return render(request , 'features.html')
 
 
-
+@login_required(login_url='/login')
 def chatbot(request):
     bot_reply = None
     user_message = ""
@@ -34,3 +66,7 @@ def chatbot(request):
         'bot_reply': bot_reply,
         'obj':obj,
     })
+
+def user_logout(request):
+  auth.logout(request)
+  return redirect('index')
